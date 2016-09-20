87d0499f56a602d4a7640f34817357363c0755f5
hayashi <hayashi@roboshin.com>
2016-09-20 20:03:47 +0900

    帰宅前保存：ぐちゃぐちゃ

--- Tcommu(Client)/Test_Client.cs
+++ Tcommu(Client)/Test_Client.cs
@@ -8,15 +8,18 @@ using System.Net.Sockets;
 
 public class Client
 {
-    //------------------------フィールド-------------------------------------------
+    //フィールド
     public NetworkStream ns { get; set; }
     public TcpClient tcp { get; set; }
     public string sendMsg { get; set; }
     public Encoding douData { get; set; }
+    public string cv{get; set;}
 
-    //------------------------接続設定---------------------------------------------
+    //接続設定:サーバと接続できるまで繰り返す
     //コンストラクタ
-    public Client(string ipOrHost, int port)
+    //接続設定
+    //var cv = new Client(ipOrHost: "127.0.0.1", port: 2001);
+    public Client()
     {
         bool again;
         do
@@ -25,7 +28,7 @@ public class Client
             try
             {
                 //TcpClientを作成し、サーバーと接続する
-                tcp = new TcpClient(ipOrHost, port);
+                tcp = new TcpClient("127.0.0.1",2001);
                 Console.WriteLine("サーバー({0}:{1})と接続しました({2}:{3})。",
                     ((IPEndPoint)tcp.Client.RemoteEndPoint).Address,
                     ((IPEndPoint)tcp.Client.RemoteEndPoint).Port,
@@ -34,16 +37,17 @@ public class Client
                 //NetworkStreamを取得する
                 ns = tcp.GetStream();
             }
-            catch (System.Net.Sockets.SocketException)
+            catch (SocketException)
             {
                 again = true;
                 Console.WriteLine("Connection Error:Serverを起動してください");
                 System.Threading.Thread.Sleep(1000);
             }
         } while (again);
+        var cv = new Client();
     }
-    //----------------------------入力設定--------------------------------------------
-    //サーバーに送信するデータを入力してもらう
+
+    //入力：任意の文字列を入力stringを出力
     public string Comment()
     {
         Console.WriteLine("文字列を入力し、Enterキーを押してください。");
@@ -53,45 +57,38 @@ public class Client
         {
             return string.Empty;
         }
-        return sendMsg;
-    }
-    //----------------------------リスト作成-------------------------------------------
-    public List<int> LData()
-    {
-        //データをストリームへ取得
-        NetworkStream stream = tcp.GetStream();
-        // List<T>クラスのインスタンス化
-        List<int> intList = new List<int>();
-        // 要素の追加
-        intList.Add(1001200);
-        intList.Add(2001100);
-        intList.Add(3001200);
-        return intList;
-    }
-    //----------------------------送信設定--------------------------------------------
-    public string CSendData(string sdata)
-    {
-        //サーバーにデータを送信する
-        //文字列をByte型配列に変換
-        douData = Encoding.UTF8;
-        byte[] sendBytes = douData.GetBytes(sendMsg + '\n');
-        //データを送信する
-        ns.Write(sendBytes, 0, sendBytes.Length);
+
         return sendMsg;
     }
 
+    //送信：stringをByteに変換し送信 
+    public string CSendData(string sdata)
+    {
+        //文字列をByte型配列に変換
+        douData = Encoding.UTF8;
+        byte[] sendBytes = douData.GetBytes(sendMsg + '\n');
+
+        //データを送信
+        ns.Write(sendBytes, 0, sendBytes.Length);
+
+        return sendMsg;
+    }
+
+    //送信：List<int> をByteに変換し送信 
     public List<int> CSendData(List<int> sendMsg)
     {
-         for (int i = 0; i < sendMsg.Count; i++)
+        for (int i = 0; i < sendMsg.Count; i++)
         {
             string strData = (sendMsg[i] + " ");
             byte[] Gdata = Encoding.UTF8.GetBytes(strData);
             ns.Write(Gdata, 0, Gdata.Length);
             Console.WriteLine(strData);
         }
+
         return sendMsg;
     }
-    //-----------------------------受信設定-----------------------------------------------
+
+    //受信：double
     public double CResceiveData()
     {
         //サーバーから送られたデータを受信する
@@ -102,39 +99,46 @@ public class Client
         {
             //データの一部を受信する
             resSize = ns.Read(resBytes, 0, resBytes.Length);
+
             //Readが0を返した時はサーバーが切断したと判断
             if (resSize == 0)
             {
                 Console.WriteLine("サーバーが切断しました。");
                 break;
             }
+
             //受信したデータを蓄積する
             ms.Write(resBytes, 0, resSize);
-            //まだ読み取れるデータがあるか、データの最後が\nでない時は、受信を続ける
-        } while (ns.DataAvailable || resBytes[resSize - 1] != '\n');
-        
+        } while (ns.DataAvailable || resBytes[resSize - 1] != '\n');           //まだ読み取れるデータがあるか、データの最後が\nでない時は、受信を続ける
+
         //受信したデータを文字列に変換
         string resMsg = douData.GetString(ms.GetBuffer(), 0, (int)ms.Length);
-        ms.Close();    
+        ms.Close();
+
         //末尾の\nを削除
         double dresMsg = double.Parse(resMsg.TrimEnd('\n'));
+
         return dresMsg;
     }
-     //----------------------------------切断設定--------------------------------------------
+
+    //切断設定
     public void CClose()
     {
         //閉じる
         ns.Close();
         tcp.Close();
         Console.WriteLine("切断しました。");
-        Console.ReadLine();
+        Console.WriteLine();
     }
-    //-----------------------------------Main-----------------------------------------------
+}
+
+class ClientMain
+{ 
+    //Main
     public static void Main()
     {
-        //接続設定
-        var cv = new Client(ipOrHost: "127.0.0.1", port: 2001);
-
+        Client cv;
+        
         //入力設定
         var CComent = cv.Comment();
         Console.WriteLine();
@@ -143,14 +147,17 @@ public class Client
             //送信設定1
             var CSendMsg1 = cv.CSendData(CComent);
             Console.WriteLine("文字送信：{0}", CSendMsg1);
+
             //受信設定
             var CReceiveMsg = cv.CResceiveData();
             Console.WriteLine("double受信:{0}", CReceiveMsg);
+
             //送信設定2
-            var SLData = cv.LData();
+            var SLData = new List<int>() { 1, 2, 3, 4, 5 };
             Console.WriteLine("List<int>送信：");
             var CSendMsg2 = cv.CSendData(SLData);
         }
+
         //切断設定
         Console.WriteLine();
         cv.CClose();
--- Test_communication/Test_Server.cs
+++ Test_communication/Test_Server.cs
@@ -5,161 +5,186 @@ using System.Collections.Generic;
 using System.Net;
 using System.Text;
 
-public class Server
+namespace ServerSys
 {
-    //------------------------フィールド-------------------------------------------
-    public System.Net.Sockets.TcpListener listener { get; set; }
-    public System.Net.Sockets.NetworkStream ns { get; set; }
-    public System.Net.Sockets.TcpClient client { get; set; }
-    public Encoding enc { get; set; }
-    public bool disconnected { get; set; }
-    public string resMsg {get; set;}
-    //------------------------接続設定---------------------------------------------
-    //コンストラクタ
-    public Server(string ipString, int port)
+    public class Server
     {
-        //ListenするIPアドレス
-        IPAddress ipAdd = IPAddress.Parse(ipString);
+        //フィールド
+        public System.Net.Sockets.TcpListener listener { get; set; }
+        public System.Net.Sockets.NetworkStream ns { get; set; }
+        public System.Net.Sockets.TcpClient client { get; set; }
+        public Encoding enc { get; set; }
+        public bool disconnected { get; set; }
+        public string resMsg { get; set; }
 
-        //TcpListenerオブジェクトを作成する
-        listener = new System.Net.Sockets.TcpListener(ipAdd, port);
-
-        //Listenを開始する
-        listener.Start();
-        Console.WriteLine("Listenを開始しました({0}:{1})。",
-            ((IPEndPoint)listener.LocalEndpoint).Address,
-            ((IPEndPoint)listener.LocalEndpoint).Port);
-
-        //接続要求があったら受け入れる
-        client = listener.AcceptTcpClient();
-        Console.WriteLine("クライアント({0}:{1})と接続しました。",
-            ((IPEndPoint)client.Client.RemoteEndPoint).Address,
-            ((IPEndPoint)client.Client.RemoteEndPoint).Port);
-
-        //NetworkStreamを取得
-        ns = client.GetStream();
-    }
-    //------------------------受信設定---------------------------------------------
-    public string SResveData1()
-    {
-        //クライアントから送られたデータを受信する
-        enc = Encoding.UTF8;
-        disconnected = false;
-        System.IO.MemoryStream ms = new System.IO.MemoryStream();
-        byte[] resBytes = new byte[256];
-        int resSize = 0;
-        do
-        {
-            //データの一部を受信する
-            resSize = ns.Read(resBytes, 0, resBytes.Length);
-            //Readが0を返した時はクライアントが切断したと判断
-            if (resSize == 0)
-            {
-                disconnected = true;
-                Console.WriteLine("クライアントが切断しました。");
-                break;
-            }
-            //受信したデータを蓄積する
-            ms.Write(resBytes, 0, resSize);
-        } while (ns.DataAvailable || resBytes[resSize - 1] != '\n');　               //まだ読み取れるデータがあるか、データの最後が\nでない時は、受信を続ける
-
-        //受信したデータを文字列に変換
-        string resMsg = enc.GetString(ms.GetBuffer(), 0, (int)ms.Length);
-        ms.Close();
-        //末尾の\nを削除
-        resMsg = resMsg.TrimEnd('\n');
-        return resMsg;
-    }
-
-    public string SResveData2()
-    {
-        //データをストリームへ取得
-        System.Net.Sockets.NetworkStream stream = client.GetStream();
-
-        //データを受け取るbyte型変数を定義（例では１バイトずつ受け取る）
-        byte[] getData = new byte[1];
-
-        //データの取得と同時に、取得したデータのバイト数も得る.引数は（受け皿,格納開始位置,受け取るバイト数）
-        int cnt;
-        //どれだけもらうかわからないので一時的に格納するリストを定義
-        List<byte> bytelist = new List<byte>();
-        //cntには受け取ったデータの長さが入る
-        while ((cnt = stream.Read(getData, 0, getData.Length)) > 0)
-        {
-            //データをリストに追加していく
-            bytelist.Add(getData[0]);
-        }
-
-        //リストに入った分だけ配列を定義
-        byte[] result = new byte[bytelist.Count];
-
-        for (int i = 0; i < result.Length; i++)
-        {
-            result[i] = bytelist[i];
-        }
-        //文字列にエンコード
-        string data = Encoding.UTF8.GetString(result);
-        return data;
-    }
-    //-----------------------送信設定----------------------------------------------
-
-    public string SSendData(string remsg)
-    {
-        //クライアントに送信する文字列を作成
-        double dresmsg = (remsg.Length)*0.123456789;
-        string sendmsg = dresmsg.ToString();
-        //文字列をbyte型配列に変換
-        byte[] sendbytes = enc.GetBytes(sendmsg + '\n');
-        //データを送信する
-        ns.Write(sendbytes, 0, sendbytes.Length);
-        //console.writeline("double送信：{0}", dresmsg);
-        return sendmsg;
-    }
-    //-------------------切断設定--------------------------------------------------
-    public void SClose()
-    {
-        //閉じる
-        ns.Close();
-        client.Close();
-        Console.WriteLine("クライアントとの接続を閉じました。");
-
-        //リスナを閉じる
-        listener.Stop();
-        Console.WriteLine("listenerを閉じました。");
-        Console.ReadLine();
-    }
-    //---------------------- Main --------------------------------------------------
-    public static void Main()
-    {
         //接続設定
-        var sv = new Server(ipString: "127.0.0.1", port: 2001); //接続処理を回したままにしておく
-
-        try
+        //コンストラクタ
+        public Server(string ipString, int port)
         {
-            //受信設定1
-            var SReceivingMsg1 = sv.SResveData1();
-            Console.WriteLine("文字受信：{0}", SReceivingMsg1);
+            //ListenするIPアドレス
+            IPAddress ipAdd = IPAddress.Parse(ipString);
 
-            //送信設定
-            //クライアントにデータを送信する
-            var SSendMsg = sv.SSendData(SReceivingMsg1);
-            Console.WriteLine("double送信：{0}", SSendMsg);
+            //TcpListenerオブジェクトを作成する
+            listener = new System.Net.Sockets.TcpListener(ipAdd, port);
 
-            //受信設定2
-            var SReceivingMsg2 = sv.SResveData2();
-            string[] Sdata = SReceivingMsg2.Split(' ');
-            Console.WriteLine("List<int>受信：");
-            foreach (string stData in Sdata)
+            //Listenを開始する
+            listener.Start();
+            Console.WriteLine("Listenを開始しました({0}:{1})。",
+                ((IPEndPoint)listener.LocalEndpoint).Address,
+                ((IPEndPoint)listener.LocalEndpoint).Port);
+
+            //接続要求があったら受け入れる
+            client = listener.AcceptTcpClient();
+            Console.WriteLine("クライアント({0}:{1})と接続しました。",
+                ((IPEndPoint)client.Client.RemoteEndPoint).Address,
+                ((IPEndPoint)client.Client.RemoteEndPoint).Port);
+
+            //NetworkStreamを取得
+            ns = client.GetStream();
+        }
+
+        //受信設定：文字
+        public string SResveData1()
+        {
+            //クライアントから送られたデータを受信する
+            enc = Encoding.UTF8;
+            disconnected = false;
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            byte[] resBytes = new byte[256];
+            int resSize = 0;
+            do
             {
-                Console.WriteLine(stData);
+                //データの一部を受信する
+                resSize = ns.Read(resBytes, 0, resBytes.Length);
+
+                //Readが0を返した時はクライアントが切断したと判断
+                if (resSize == 0)
+                {
+                    disconnected = true;
+                    Console.WriteLine("クライアントが切断しました。");
+                    break;
+                }
+                //受信したデータを蓄積する
+                ms.Write(resBytes, 0, resSize);
+            } while (ns.DataAvailable || resBytes[resSize - 1] != '\n');                //まだ読み取れるデータがあるか、データの最後が\nでない時は、受信を続ける
+
+            //受信したデータ(Byte)をList<int>に変換
+            string resMsg = enc.GetString(ms.GetBuffer(), 0, (int)ms.Length);
+
+            ms.Close();
+
+            //末尾の\nを削除
+            resMsg = resMsg.TrimEnd('\n');
+            //List<int> SintList = new List<int>();
+            //for (int i = 0; i<)
+            return resMsg;
+        }
+
+        //受信設定：List
+        public List<int> SResveData2()
+        {
+            //データをストリームへ取得
+            System.Net.Sockets.NetworkStream stream = client.GetStream();
+
+            //データを受け取るbyte型変数を定義（例では１バイトずつ受け取る）
+            byte[] getData = new byte[1];
+
+            //データの取得と同時に、取得したデータのバイト数も得る.引数は（受け皿,格納開始位置,受け取るバイト数）
+            int cnt;
+
+            //どれだけもらうかわからないので一時的に格納するリストを定義
+            List<byte> bytelist = new List<byte>();
+
+            //cntには受け取ったデータの長さが入る
+            while ((cnt = stream.Read(getData, 0, getData.Length)) > 0)
+            {
+                //データをリストに追加していく
+                bytelist.Add(getData[0]);
             }
 
-            //切断設定
-            sv.SClose();
+            //リストに入った分だけ配列を定義
+            byte[] result = new byte[bytelist.Count];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = bytelist[i];
+            }
+
+            ////文字列にエンコード
+            //string data = Encoding.UTF8.GetString(result);
+
+            List<int> data = result.ConvertAll(x => int.Parse(x));
+
+            return data;
         }
-        catch (Exception)
+
+        //送信設定：double
+
+        public string SSendData(string remsg)
         {
-            sv.SClose();
+            //クライアントに送信する文字列を作成
+            double dresmsg = (remsg.Length) * 0.123456789;
+            string sendmsg = dresmsg.ToString();
+            //文字列をbyte型配列に変換
+            byte[] sendbytes = enc.GetBytes(sendmsg + '\n');
+            //データを送信する
+            ns.Write(sendbytes, 0, sendbytes.Length);
+
+            return sendmsg;
+        }
+
+        //切断設定
+        public void SClose()
+        {
+            //閉じる
+            ns.Close();
+            client.Close();
+            Console.WriteLine("クライアントとの接続を閉じました。");
+
+            //リスナを閉じる
+            listener.Stop();
+            Console.WriteLine("listenerを閉じました。");
+            Console.WriteLine();
+        }
+    }
+
+    class ServerMain
+    {
+        //Main
+        public static void Main()
+        {
+            //接続設定
+            var sv = new Server(ipString: "127.0.0.1", port: 2001); //接続処理を回したままにしておく
+            Console.WriteLine();
+
+            try
+            {
+                //受信設定
+                var SReceivingMsg1 = sv.SResveData1();
+                Console.WriteLine("文字受信：{0}", SReceivingMsg1);
+
+                //送信設定
+                //クライアントにデータを送信する
+                var SSendMsg = sv.SSendData(SReceivingMsg1);
+                Console.WriteLine("double送信：{0}", SSendMsg);
+
+                //受信設定
+                var SReceivingMsg2 = sv.SResveData2();
+                //string[] Sdata = SReceivingMsg2.Split(' ');
+                Console.WriteLine("List<int>受信：{0}",SReceivingMsg2);
+                //foreach (List<int> stData in SReceivingMsg2)
+                //{
+                //    Console.WriteLine(stData);
+                //}
+
+                //切断設定
+                sv.SClose();
+            }
+
+            catch (Exception)
+            {
+                sv.SClose();
+            }
         }
     }
 }
